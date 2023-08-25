b66beced90689740bffbe887a95a2b39db5d9110
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2023-08-25 15:30:02 +0300

    Follow kernel style

--- hello-1/hello-1.c
+++ hello-1/hello-1.c
@@ -4,12 +4,16 @@
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/printk.h> /* Needed for pr_info() */
 
-int init_module(void) {
-  pr_info("Hello world 1.\n");
-  /* A non 0 return means init_module failed; module can't be loaded. */
-  return 0;
+int init_module(void)
+{
+	pr_info("Hello world 1.\n");
+	/* A non 0 return means init_module failed; module can't be loaded. */
+	return 0;
 }
 
-void cleanup_module(void) { pr_info("Goodbye world 1.\n"); }
+void cleanup_module(void)
+{
+	pr_info("Goodbye world 1.\n");
+}
 
 MODULE_LICENSE("GPL");
